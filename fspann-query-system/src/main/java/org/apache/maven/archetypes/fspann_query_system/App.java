94ae356cf2cc662817fa4d3918667aa2e6ec97cb
Mehran_Memon <mehran@stu.xidian.edu.cn>
2025-02-18 18:47:16 +0800

    Editing pom.xml and Setting up new dependencies.

---
+++ fspann-query-system/src/main/java/org/apache/maven/archetypes/fspann_query_system/App.java
@@ -0,0 +1,10 @@
+package org.apache.maven.archetypes.fspann_query_system;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
