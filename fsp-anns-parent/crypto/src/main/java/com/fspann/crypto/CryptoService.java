ecf7bd8f19c09525b1f49f4f73eed9be2eb18d55
Mehran-Memon <mehran@stu.xidian.edu.cn>
2025-06-15 04:05:43 +0800

    Updated Test Files AesGcmCryptoService.class, CryptoService.class, CryptoService.java and others

--- fsp-anns-parent/crypto/src/main/java/com/fspann/crypto/CryptoService.java
+++ fsp-anns-parent/crypto/src/main/java/com/fspann/crypto/CryptoService.java
@@ -1,4 +1,3 @@
-// in crypto module
 package com.fspann.crypto;
 
 import com.fspann.common.EncryptedPoint;
@@ -6,9 +5,9 @@ import javax.crypto.SecretKey;
 
 public interface CryptoService {
     EncryptedPoint encryptToPoint(String id, double[] vector, SecretKey key);
-    double[] decryptFromPoint(EncryptedPoint point, SecretKey key);
-
-    // NEW: decrypt a standalone query payload with its IV
-    double[] decryptQuery(byte[] ciphertext, byte[] iv, SecretKey key) throws Exception;
+    double[] decryptFromPoint(EncryptedPoint pt, SecretKey key);
+    byte[] encrypt(double[] vector, SecretKey key, byte[] iv);
+    double[] decryptQuery(byte[] encryptedQuery, byte[] iv, SecretKey key);
+    EncryptedPoint reEncrypt(EncryptedPoint pt, SecretKey newKey);
+    byte[] generateIV();
 }
-
